d3b3246e04c9c8d4b7aaa1d7902dde344552fb64
Mix-Liten <mixliten@gmail.com>
2023-05-11 16:32:21 +0800

    feat: update rootLayout at md

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -10,20 +10,22 @@ export default function RootLayout() {
       <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
         <GridItem
           as="aside"
-          colSpan={{ base: 2, xl: 1 }}
           bg="yellow.200"
           minH={{ md: 'calc(100vh - 4.5rem)' }}
-          p={{ base: '20px', md: '30px' }}
+          p='20px'
+          w='120px'
           display={{ base: 'none', md: 'block' }}
+          pos="fixed"
         >
           <Sidebar />
         </GridItem>
         <GridItem
           as="main"
-          colSpan={{ base: 6, md: 4, xl: 5 }}
+          colSpan={{ base: 8 }}
           bg="yellow.50"
           p="40px"
-          minH={{ base: 'calc(100vh - 4.5rem)', md: 'auto' }}
+          ml={{base: 'none', md: '120px'}}
+          minH={{ base: 'calc(100vh - 4.5rem)' }}
         >
           <Outlet />
         </GridItem>
