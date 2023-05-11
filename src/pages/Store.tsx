a9e42922710914019f9b3f23bebded5e56845fca
Mix-Liten <mixliten@gmail.com>
2023-05-11 16:17:40 +0800

    init static data

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import storeItems from '../data/items.json'
+
 export default function Store() {
-  return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      {storeItems.map(item => (
+        <Box>{JSON.stringify(item)}</Box>
+      ))}
+    </>
+  )
 }
